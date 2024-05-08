92ab9ba1ee1ee6643cf5c65cd9bd045c362e87a3
rizanpm <rizanrafeeque786@gmail.com>
2024-05-08 12:30:16 +0530

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>Welcome to our online emporium, where the world of shopping unfolds at your fingertips. Dive into a realm of endless possibilities, where every click opens doors to a treasure trove of products to delight and inspire. From the latest fashion trends to cutting-edge gadgets, from cozy home essentials to thrilling outdoor gear, our virtual shelves are stocked with everything you need and more. Immerse yourself in the convenience of modern shopping, where convenience meets quality, and satisfaction is just a checkout away.</p>
+            <p>
+            Step into a world of digital delights at our online marketplace, where the thrill of discovery awaits. Explore a curated collection of products tailored to suit every need and desire, from fashion-forward apparel to innovative gadgets and beyond. With seamless navigation and secure transactions, we bring the joy of shopping right to your screen. Embrace the convenience of modern commerce and embark on a journey of endless possibilities, all from the comfort of your own home.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
